ce136b15417f4663d1bcd5cda38044da32694ee9
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-05-11 12:30:48 +0530

    Add files via upload

---
+++ BHARGI7.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  float celsuis,fahrenheit;
+  clrscr();
+  printf("\n enter the temparature in celsius:");
+  scanf("%f",celsuis);
+  fanrenheit=(1.8*celsius)+32;
+  printf("\n temperature in fahrenheit ;%f",fahrenheit);
+  getch();
+}
+
